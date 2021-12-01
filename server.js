49bb3c9accdf5bce7a4f8a38093a5b8fbd6b09ff
me-marchi <me.marchi@outlook.com>
2021-12-01 15:53:40 -0300

    erro 4

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const app = require("./src/app");
-const port = process.env.PORT || 5432;
+const port = process.env.PORT || 8786;
 
 app.listen(port, () => {
     console.log("Servidor rodando na porta " + port)
